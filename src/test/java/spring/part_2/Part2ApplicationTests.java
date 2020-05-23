bf0997e526ad6f3f99c237465be0121b60bfb7cf
shein dima <msh86.02.18@rambler.ru>
2020-05-23 10:46:28 +0500

    Initial commit

---
+++ src/test/java/spring/part_2/Part2ApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.part_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Part2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
